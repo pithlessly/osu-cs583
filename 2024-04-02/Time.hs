1fd4570651f3a74948125b84cad8e5efa15ca720
pithlessly <79002325+pithlessly@users.noreply.github.com>
2024-04-02 10:51:21 -0700

    IntSet

--- 2024-04-02/Time.hs
+++ 2024-04-02/Time.hs
@@ -11,3 +11,28 @@ secs (Minutes m) = m * 60
 
 instance Eq Time where
   (==) = (==) `on` secs
+
+data IntSet = ESet | Ins Int IntSet
+
+toList :: IntSet -> [Int]
+toList ESet = []
+toList (Ins i s) = insert i (toList s) where
+  insert i [] = [i]
+  insert i (n : ns)
+    | i < n  = i : n : ns
+    | i > n  = n : insert i ns
+    | i == n = n : ns
+
+-- potential instance:
+-- (==) = (==) `on` toList
+
+member :: Int -> IntSet -> Bool
+member n ESet = False
+member n (Ins m s) = (n == m) || member n s
+
+subset :: IntSet -> IntSet -> Bool
+subset ESet _ = True
+subset (Ins n s) s' = member n s' && subset s s'
+
+instance Eq IntSet where
+  s1 == s2 = subset s1 s2 && subset s2 s1
