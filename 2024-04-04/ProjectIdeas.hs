af4a1718cf842af01c48faca27a7e9c1d69459e7
pithlessly <79002325+pithlessly@users.noreply.github.com>
2024-04-11 01:03:56 -0700

    add RRB API example with property tests

--- 2024-04-04/ProjectIdeas.hs
+++ 2024-04-04/ProjectIdeas.hs
@@ -1,3 +1,5 @@
+module ProjectIdeas where
+
 type Var = String
 
 data Formula
@@ -37,18 +39,95 @@ satExamples =
 -- classProject = satisfiable
 
 data Rrb a = Rrb {- abstract -}
-data RrbAPI a = RrbAPI {
-    _empty :: Rrb a,
-    _singleton :: a -> Rrb a,
-    _fromList :: [a] -> Rrb a,
-    _cons :: a -> Rrb a -> Rrb a,
-    _snoc :: Rrb a -> a -> Rrb a,
-    _concat :: Rrb a -> Rrb a -> Rrb a,
-    _index :: Int -> Rrb a -> a,
-    _length :: Rrb a -> Int,
-    _traverse :: forall f b. Applicative f => (a -> f b) -> Rrb a -> f (Rrb b)
-  }
 
--- type In = ()
--- type Out = forall a. RrbAPI a
--- classProject () = RrbAPI { {- TODO -} }
+empty :: Rrb a
+empty = Rrb
+
+singleton :: a -> Rrb a
+singleton _ = Rrb
+
+fromList :: [a] -> Rrb a
+fromList _ = Rrb
+
+toList :: Rrb a -> [a]
+toList _ = []
+
+cons, snoc :: a -> Rrb a -> Rrb a
+cons _ _ = Rrb
+snoc _ _ = Rrb
+
+appendV :: Rrb a -> Rrb a -> Rrb a
+appendV _ _ = Rrb
+
+lengthV :: Rrb a -> Int
+lengthV _ = 0
+
+index :: Int -> Rrb a -> a
+index _ _ = error "out of bounds"
+
+traverseV :: Applicative f => (a -> f b) -> Rrb a -> f (Rrb b)
+traverseV _ _ = pure Rrb
+
+tests :: [IO Bool]
+tests =
+  [ test "empty" ==> (
+      toList empty == ([] :: [Int]))
+
+  , test "singleton" int ==> \n ->
+      toList (singleton n) == [n]
+
+  , test "fromList" list ==> \xs ->
+      toList (fromList xs) == xs
+
+  , test "cons" int rrb ==> \x xs ->
+      toList (cons x xs) == x : toList xs
+
+  , test "snoc" int rrb ==> \x xs ->
+      toList (snoc x xs) == toList xs ++ [x]
+
+  , test "append" rrb rrb ==> \xs ys ->
+      toList (appendV xs ys) == toList xs ++ toList ys
+
+  , test "length" rrb ==> \xs ->
+      lengthV xs == length (toList xs)
+
+  , test "index" int rrb ==> \i xs ->
+      let xs' = toList xs in
+      not (0 <= i && i < length xs') ||
+        (index i xs == xs' !! i)
+
+  , test "traverse" rrb ==> \xs ->
+      let f n = (show n, n + 1) in
+      (toList <$> traverseV f xs) == traverse f (toList xs)
+  ]
+  where
+    int  = testArgument "an integer"         (\x -> x :: Int)
+    list = testArgument "a list of integers" (\x -> x :: [Int])
+    rrb  = testArgument "a vector"           (\x -> fromList x :: Rrb Int)
+
+-- ================================= --
+-- property testing fluent interface --
+-- ================================= --
+
+type Sat a = (a -> a) -> a
+type Chain a b = (String, IO (a -> b))
+
+test :: String -> (Chain a a -> r) -> r
+test name = \k -> k (name, pure applyNoArgs)
+  where applyNoArgs = id
+
+testArgument :: Read s => String -> (s -> t) -> Chain a (t -> b) -> (Chain a b -> r) -> r
+testArgument ty conv (name, mkApplicator) = \k -> k (name, do
+  applyArgs <- mkApplicator
+  putStr $ "Please enter " ++ ty ++ ": "
+  v <- conv . read <$> getLine
+  pure (($ v) . applyArgs))
+
+(==>) :: Sat (Chain a Bool) -> a -> IO Bool
+chain ==> f = do
+  let (name, mkApplicator) = chain id
+  putStrLn $ let s = "Test " ++ show name in replicate (length s) '=' ++ "\n" ++ s
+  applyArgs <- mkApplicator
+  let success = applyArgs f
+  putStrLn $ "Test " ++ show name ++ if success then " passed" else " failed"
+  pure success
